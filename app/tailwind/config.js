dc5e20239ed6472070970c3c813f6652144577ca
achraf <achrafatauil@hotmail.com>
2020-10-11 20:23:47 +0200

    Install & setup tailwind.css

---
+++ app/tailwind/config.js
@@ -0,0 +1,35 @@
+/*global module*/
+
+module.exports = {
+  purge: [],
+  theme: {
+        screens: {
+          sm: '640px',
+          md: '768px',
+          lg: '1024px',
+          xl: '1440px',
+        },
+      container: {
+          center: true,
+        },
+      extend: {
+        colors: {
+          gray: {
+            '100': '#f8f8f8',
+            '200': '#eeeeee',
+            '300': '#e0e0e0',
+            '400': '#bdbdbd',
+            '500': '#9e9e9e',
+            '600': '#757575',
+            '700': '#616161',
+            '800': '#424242',
+            '900': '#212121',
+          }
+        }
+      }
+
+  },
+
+  variants: {},
+  plugins: [],
+}
